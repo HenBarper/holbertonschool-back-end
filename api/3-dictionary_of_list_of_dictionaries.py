ed1796367534f2e70499535e4feda90b9ef3599a
Ben Harper <benjaminitonio@gmail.com>
2023-07-21 14:17:54 -0500

    chmoded 2

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Dictionary of list of dictionaries """
+import json
 import requests
 import sys
 
@@ -7,38 +8,35 @@ import sys
 def record_all_tasks():
     """gather and print api data"""
 
+    user_data = requests.get('https://jsonplaceholder.typicode.com/users').json()
     todo_data = requests.get('https://jsonplaceholder.typicode.com/todos').json()
 
     counter = 0
-    # id_list = []
-    # task_list = []
-    # complete_status_list = []
-    # username_list = []
+    task_list = []
+    complete_status_list = []
+    username_list = []
     for thing in todo_data:
         if thing.get("userId") > counter:
             counter += 1
-        # id_list.append(counter)
-        # task_list.append(thing.get("title"))
-        # complete_status_list.append(thing.get("completed"))
-        # username_list.append(thing.get("username"))
+        task_list.append(thing.get("title"))
+        complete_status_list.append(thing.get("completed"))
+        username_list.append(thing.get("username"))
     
     todo_list = []
     for i in range(1, counter):
         todo_list.append(requests.get('https://jsonplaceholder.typicode.com/todos',
                              params={"userId": i}).json())
 
+    json_dict = {}
     for i in range(len(todo_list)):
         json_list = []
         new_dict = {
-                "username": username_list[j],
-                "task": task_list[j],
-                "completed": complete_status_list[j],
+                "username": username_list[i],
+                "task": task_list[i],
+                "completed": complete_status_list[i],
             }
         json_list.append(new_dict)
-        json_dict = {
-        f"{USER_ID}": json_list
-        }
-            
+        json_dict[f'{i}'] = json_list            
 
     # for i in range(len(todo_list)):
     #     print(todo_list[i])
@@ -56,10 +54,10 @@ def record_all_tasks():
     #     }
     #     json_list.append(new_dict)
 
-    # json_object = json.dumps(json_dict)
+    json_object = json.dumps(json_dict)
 
-    # with open('todo_all_employees.json', 'w') as f:
-    #     f.write(json_object)
+    with open('todo_all_employees.json', 'w') as f:
+        f.write(json_object)
 
 
 if __name__ == "__main__":
